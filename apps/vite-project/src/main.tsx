f3bb3a239331be868cf65b5c3391ff07ed8c17ba
akkadaya <1834445+akkadaya@users.noreply.github.com>
2024-09-18 00:12:17 +1000

    add biome

--- apps/vite-project/src/main.tsx
+++ apps/vite-project/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+// biome-ignore lint/style/noNonNullAssertion: required by Vite
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
